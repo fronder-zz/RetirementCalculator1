09b1b0635809542e9f0e1b71c7f2186d7d995989
fronder <khassanbeck@gmail.com>
2020-08-29 18:43:50 +0500

    Initial Commit

---
+++ RetirementCalculator1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetirementCalculator1
+//
+//  Created by Hasan on 8/29/20.
+//  Copyright © 2020 Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
